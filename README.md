edb85f0aebd6c10119175fcd6a0c44ed42e7920f
Jubi <Jovane.Hogg@gmail.com>
2024-05-03 19:54:17 -0400

    dockerized

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # python-flask
 basic flask app 
+
+# Docker commands
+docker build -t flask-app . 
+docker run -d -p 5000:5000 flask-app 
--- app.py
+++ app.py
@@ -1,8 +1,22 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello():
+#     return 'Jubi\'s, Python World!'
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
-    return 'Jubi\'s, Python World!'
+def hello_geek():
+    return '<h1>Hello from Jubi</h2>'
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
